6fdb2f860e2f502ac51bf0f873e7a192e0eb16ac
heavenly-zy <1457715339@qq.com>
2019-11-09 22:58:17 +0800

    封装成为组件

--- main.js
+++ main.js
@@ -1,40 +1,44 @@
-const $ = x => document.querySelector(x)
-const $$ = x => document.querySelectorAll(x)
+class Carousel {
+    constructor(root) {
+        this.root = root
+        this.dotsWrapper = root.querySelector('.dots')
+        this.dots = Array.from(root.querySelectorAll('.dots > span'))
+        this.panels = Array.from(root.querySelectorAll('.panels > a'))
+        this.pre = root.querySelector('.action .pre')
+        this.next = root.querySelector('.action .next')
 
-$('.carousel .dots').onclick = function (e) {
-    if (e.target.tagName !== 'SPAN') return
-    let index = Array.from($$('.carousel .dots span')).indexOf(e.target)
-    // 切换dots
-    $$('.carousel .dots span').forEach(dot => dot.classList.remove('active'))
-    $$('.carousel .dots span')[index].classList.add('active')
-    // 切换图片
-    $$('.carousel .panels a').forEach(panel => panel.style.zIndex = 1)
-    $$('.carousel .panels a')[index].style.zIndex = 10
+        this.bind()
+    }
+
+    bind() {
+        this.dotsWrapper.onclick = (e) => {
+            if (e.target.tagName !== 'SPAN') return
+            let index = this.dots.indexOf(e.target)
+            this.setDots(index)
+            this.setPanels(index)
+        }
+        this.pre.onclick = (e) => {
+            let index = this.dots.indexOf(this.dotsWrapper.querySelector('span.active'))
+            index = (index - 1 + this.dots.length) % this.dots.length // 0 ~ 3
+            this.setDots(index)
+            this.setPanels(index)
+        }
+
+        this.next.onclick = (e) => {
+            let index = this.dots.indexOf(this.dotsWrapper.querySelector('span.active'))
+            index = (index + 1) % this.dots.length // 0 ~ 3
+            this.setDots(index)
+            this.setPanels(index)
+        }
+    }
+    setDots(index) {
+        this.dots.forEach(dot => dot.classList.remove('active'))
+        this.dots[index].classList.add('active')
+    }
+    setPanels(index) {
+        this.panels.forEach(panel => panel.style.zIndex = 1)
+        this.panels[index].style.zIndex = 10
+    }
 }
 
-$('.pre').onclick = function (e) {
-    let index = Array.from($$('.carousel .dots span')).indexOf($('.carousel .dots span.active'))
-    index = (index - 1 + $$('.carousel .dots span').length) % $$('.carousel .dots span').length // 0 ~ 3
-    // 切换dots
-    setDots(index)
-    // 切换图片
-    setPanels(index)
-}
-
-$('.next').onclick = function (e) {
-    let index = Array.from($$('.carousel .dots span')).indexOf($('.carousel .dots span.active'))
-    index = (index + 1) % $$('.carousel .dots span').length // 0 ~ 3
-    // 切换dots
-    setDots(index)
-    // 切换图片
-    setPanels(index)
-}
-
-function setDots(index) {
-    $$('.carousel .dots span').forEach(dot => dot.classList.remove('active'))
-    $$('.carousel .dots span')[index].classList.add('active')
-}
-function setPanels(index) {
-    $$('.carousel .panels a').forEach(panel => panel.style.zIndex = 1)
-    $$('.carousel .panels a')[index].style.zIndex = 10
-}
+document.querySelectorAll('.carousel').forEach(x => { new Carousel(x) })
